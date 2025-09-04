2bbc3d366a424c0fbed38815a839cc75fb87fe85
CondeSun <michael.fritsch@bayformatic.de>
2025-09-04 08:28:58 +0200

    activated custom init lua + moved mapping

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+--
+
+-- Custom KeyBindings
+-- Open Neotree via Leader Key
+vim.keymap.set('n', '<leader>n', '<cmd>:Neotree<cr>', { desc = 'Open [N]eotree' })
+
+-- Provide Prettier Shortcut via Leader Key
+vim.keymap.set('n', '<leader>fp', '<cmd>silent %!prettier --stdin-filepath %<cr>', { desc = 'Format with [P]rettier' })
+
 return {}
